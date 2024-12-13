18ed7aa39a1f5a1a109b2e768b7205d3864abe56
yiminwook <grs0416@gmail.com>
2024-12-13 12:03:35 +0900

    #TST#NEW - SSE event

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -4,162 +4,6 @@ import Editor from "@/components/Editor";
 import { useColorScheme } from "@mantine/hooks";
 import { useEffect, useState } from "react";
 import Banner from "./Banner";
-
-const DUMMY_DATA = [
-  {
-    id: "a1",
-    imgSrc:
-      "https://vexk.io/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fplu9banner.8acf9612.png&w=1920&q=75",
-    alt: "plu9",
-    text: "ANNOUNCEMENT_2",
-    url: "https://vexk.zendesk.com/hc/en-us/articles/11236229144591",
-  },
-  {
-    id: "b1",
-    imgSrc:
-      "https://vexk.io/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FblockmoonieoBannner.5962d0e0.png&w=1920&q=75",
-    alt: "ait announcement",
-    text: "ANNOUNCEMENT_1",
-    url: "https://vexk.zendesk.com/hc/en-us/articles/11236229144591",
-  },
-  {
-    id: "c1",
-    imgSrc:
-      "https://vexk.io/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FaitBanner.66e31911.png&w=1920&q=75",
-    alt: "BTBbannerImg",
-    text: "ANNOUNCEMENT_4",
-    url: "https://vexk.zendesk.com/hc/en-us/articles/11236229144591",
-  },
-  {
-    id: "d1",
-    imgSrc:
-      "https://vexk.io/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FBTBbanner.be31f21d.png&w=1920&q=75",
-    alt: "referralBanner",
-    text: "ANNOUNCEMENT_3",
-    url: "https://vexk.zendesk.com/hc/en-us/articles/11236229144591",
-  },
-  {
-    id: "a2",
-    imgSrc:
-      "https://vexk.io/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fplu9banner.8acf9612.png&w=1920&q=75",
-    alt: "plu9",
-    text: "ANNOUNCEMENT_2",
-    url: "https://vexk.zendesk.com/hc/en-us/articles/11236229144591",
-  },
-  {
-    id: "b2",
-    imgSrc:
-      "https://vexk.io/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FblockmoonieoBannner.5962d0e0.png&w=1920&q=75",
-    alt: "ait announcement",
-    text: "ANNOUNCEMENT_1",
-    url: "https://vexk.zendesk.com/hc/en-us/articles/11236229144591",
-  },
-  {
-    id: "c2",
-    imgSrc:
-      "https://vexk.io/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FaitBanner.66e31911.png&w=1920&q=75",
-    alt: "BTBbannerImg",
-    text: "ANNOUNCEMENT_4",
-    url: "https://vexk.zendesk.com/hc/en-us/articles/11236229144591",
-  },
-  {
-    id: "d2",
-    imgSrc:
-      "https://vexk.io/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FBTBbanner.be31f21d.png&w=1920&q=75",
-    alt: "referralBanner",
-    text: "ANNOUNCEMENT_3",
-    url: "https://vexk.zendesk.com/hc/en-us/articles/11236229144591",
-  },
-  {
-    id: "a3",
-    imgSrc:
-      "https://vexk.io/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fplu9banner.8acf9612.png&w=1920&q=75",
-    alt: "plu9",
-    text: "ANNOUNCEMENT_2",
-    url: "https://vexk.zendesk.com/hc/en-us/articles/11236229144591",
-  },
-  {
-    id: "b3",
-    imgSrc:
-      "https://vexk.io/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FblockmoonieoBannner.5962d0e0.png&w=1920&q=75",
-    alt: "ait announcement",
-    text: "ANNOUNCEMENT_1",
-    url: "https://vexk.zendesk.com/hc/en-us/articles/11236229144591",
-  },
-  {
-    id: "c3",
-    imgSrc:
-      "https://vexk.io/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FaitBanner.66e31911.png&w=1920&q=75",
-    alt: "BTBbannerImg",
-    text: "ANNOUNCEMENT_4",
-    url: "https://vexk.zendesk.com/hc/en-us/articles/11236229144591",
-  },
-  {
-    id: "d3",
-    imgSrc:
-      "https://vexk.io/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FBTBbanner.be31f21d.png&w=1920&q=75",
-    alt: "referralBanner",
-    text: "ANNOUNCEMENT_3",
-    url: "https://vexk.zendesk.com/hc/en-us/articles/11236229144591",
-  },
-  {
-    id: "a4",
-    imgSrc:
-      "https://vexk.io/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fplu9banner.8acf9612.png&w=1920&q=75",
-    alt: "plu9",
-    text: "ANNOUNCEMENT_2",
-    url: "https://vexk.zendesk.com/hc/en-us/articles/11236229144591",
-  },
-  {
-    id: "b4",
-    imgSrc:
-      "https://vexk.io/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FblockmoonieoBannner.5962d0e0.png&w=1920&q=75",
-    alt: "ait announcement",
-    text: "ANNOUNCEMENT_1",
-    url: "https://vexk.zendesk.com/hc/en-us/articles/11236229144591",
-  },
-  {
-    id: "c4",
-    imgSrc:
-      "https://vexk.io/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FaitBanner.66e31911.png&w=1920&q=75",
-    alt: "BTBbannerImg",
-    text: "ANNOUNCEMENT_4",
-    url: "https://vexk.zendesk.com/hc/en-us/articles/11236229144591",
-  },
-  {
-    id: "d4",
-    imgSrc:
-      "https://vexk.io/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FBTBbanner.be31f21d.png&w=1920&q=75",
-    alt: "referralBanner",
-    text: "ANNOUNCEMENT_3",
-    url: "https://vexk.zendesk.com/hc/en-us/articles/11236229144591",
-  },
-  {
-    id: "a5",
-    imgSrc:
-      "https://vexk.io/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fplu9banner.8acf9612.png&w=1920&q=75",
-    alt: "plu9",
-    text: "ANNOUNCEMENT_2",
-    url: "https://vexk.zendesk.com/hc/en-us/articles/11236229144591",
-  },
-  {
-    id: "b5",
-    imgSrc:
-      "https://vexk.io/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FblockmoonieoBannner.5962d0e0.png&w=1920&q=75",
-    alt: "ait announcement",
-    text: "ANNOUNCEMENT_1",
-    url: "https://vexk.zendesk.com/hc/en-us/articles/11236229144591",
-  },
-  {
-    id: "c5",
-    imgSrc:
-      "https://vexk.io/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FaitBanner.66e31911.png&w=1920&q=75",
-    alt: "BTBbannerImg",
-    text: "ANNOUNCEMENT_4",
-    url: "https://vexk.zendesk.com/hc/en-us/articles/11236229144591",
-  },
-];
-
 export default function Home() {
   const [cookie, setCookie] = useState("");
   const color = useColorScheme();
@@ -169,9 +13,34 @@ export default function Home() {
     setCookie(document.cookie);
   }, []);
 
+  useEffect(() => {
+    //SSE연결 로직
+    const eventSource = new EventSource(
+      "https://stockdatar.finup.co.kr/api/v1/sse?types=3&app=Finance"
+    );
+
+    eventSource.addEventListener("theme", (e) => {
+      //'new_thread' 이벤트가 오면 할 동작
+      console.log("theme", e.data);
+    });
+
+    eventSource.addEventListener("price", (e) => {
+      //'new_thread' 이벤트가 오면 할 동작
+      console.log("price", e.data);
+    });
+
+    eventSource.onerror = () => {
+      //에러 발생시 할 동작
+      eventSource.close(); //연결 끊기
+    };
+
+    return () => {
+      eventSource.close();
+    };
+  }, []);
+
   return (
     <AppShellMain>
-      <Banner data={DUMMY_DATA} />
       <form>
         <TextInput label="First name" />
         <TextInput label="Last name" mt="md" />
